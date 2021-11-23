2b3c45bc1ebdd7b080743ce9ebafffee4b65d130
tivorlu <90481970+tivorlu@users.noreply.github.com>
2021-11-23 20:52:16 +0300

    Row ile düzenlenmedi, alt alta bırakıldı. Güseloldu.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 import 'constants.dart';
 
-void main () => runApp(BilgiTesti());
+void main() => runApp(BilgiTesti());
 
 class BilgiTesti extends StatelessWidget {
   @override
@@ -14,10 +13,10 @@ class BilgiTesti extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.black,
         body: SafeArea(
-            child: Padding(padding: EdgeInsets.symmetric(horizontal: 10.0),
-              child: SoruSayfasi(),
+          child: Padding(padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: SoruSayfasi(),
 
-            ),
+          ),
         ),
       ),
     );
@@ -27,7 +26,7 @@ class BilgiTesti extends StatelessWidget {
 class SoruSayfasi extends StatefulWidget {
   @override
   _SoruSayfasiState createState() => _SoruSayfasiState();
-  }
+}
 
 class _SoruSayfasiState extends State<SoruSayfasi> {
 
@@ -43,77 +42,62 @@ class _SoruSayfasiState extends State<SoruSayfasi> {
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Expanded(
-          flex: 4,
-          child: Padding(
-            padding: EdgeInsets.all(10.0),
-            child: Center(
-              child: Text(
-                'Bilgi Testi Soruları',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.white,
-                ),
-              ),
+      Expanded(
+      flex: 4,
+      child: Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Center(
+          child: Text(
+            'Bilgi Testi Soruları',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.white,
             ),
           ),
         ),
-        Padding(padding: EdgeInsets.symmetric(horizontal: 6.0),
-        child: Row(
-          children: [
-          Expanded(
+      ),
+    ),
+    Row(
+    children: secimler,
+    ),
+        Expanded(
+          flex:1,
             child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 6.0),
-            child: ElevatedButton(
-
+            padding: const EdgeInsets.all(10.0),
+              child: ElevatedButton(
                 onPressed: (){
                   setState(() {
                     secimler.add(kYanlisIconu);
-                  });
-
-
-                },
+                  }); //setstate
+                  }, //onpressed
                 child: Icon(
                   Icons.thumb_down,
                   size: 30.0,
                   color: Colors.white,
-                ),
-            ),
-          ),
-          ),
-            Row(
-              children: secimler,
-            ),
-            Expanded(
-              flex:1,
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 6.0),
-                child: Row(
-                  children: [
-                    Expanded( child:Padding(padding: EdgeInsets.symmetric(horizontal: 6),
 
-                      child: ElevatedButton(
-
-                        child: Icon(Icons.thumb_up,
-                          size: 30.0,
-                          color: Colors.white,
-                        ),
-                        onPressed: (){
-                          setState(() {
-                            secimler.add(kDogruIconu);
-                          });
-                        },
-                      ),
-                    ),
-                    ),
-                  ],
-                ),
+              ),)
             ),
-            ),
-        ],),
         ),
-      ],
+    Expanded(
+      flex: 1,
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: ElevatedButton(
+        onPressed: (){
+          setState(() {
+            secimler.add(kDogruIconu);
+          });
+        },
+        child: Icon(
+        Icons.thumb_up,
+        size: 30.0,
+        color: Colors.white,
+        ),
+        ),
+      ),
+    ),
+        ],
     );
   }
 }
